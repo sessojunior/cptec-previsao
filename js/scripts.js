672459105e44effb3bba220d72444b6c75f2589d
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-05-10 17:18:37 -0300

    Add dialog for images

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,48 @@
-function changeRegion(dataId) {
+function changeRegion(item) {
 	let regions = document.querySelectorAll(".region");
 	for (let i = 0; i < regions.length; i++) {
 		regions[i].classList.add("hidden");
-		console.log(regions[i]);
 	}
 	let subtitleLinks = document.querySelectorAll(".subtitle-links li a");
 	for (let i = 0; i < subtitleLinks.length; i++) {
 		subtitleLinks[i].classList.remove("active");
 	}
-	document.getElementsByClassName(dataId.dataset.region)[0].classList.remove("hidden");
-	dataId.classList.add("active");
+	document.getElementsByClassName(item.dataset.region)[0].classList.remove("hidden");
+	item.classList.add("active");
+}
+
+function openImage(imageSrc, item) {
+	const figureBtn = document.querySelector("#" + item);
+	document.querySelector(".dialog-figure").setAttribute("src", imageSrc);
+	let figureBtns = document.querySelectorAll(".figure-btn");
+	for (let i = 0; i < figureBtns.length; i++) {
+		figureBtns[i].classList.remove("active");
+	}
+	figureBtn.classList.add("active");
+}
+
+function openDialog(item) {
+	const title = document.querySelector(".dialog-title");
+	const content = document.querySelector(".dialog-content");
+	const model = item.dataset.model;
+	const variable = item.dataset.variable;
+	const urlBase = item.dataset.urlBase;
+	const hs = item.dataset.hs.split(" ");
+	title.innerHTML = item.dataset.title;
+	let src = urlBase.replace("[model]", model).replace("[variable]", variable).replace("[hs]", hs[0]);
+	let text = `<img class="dialog-figure" src="${src}" alt="${model}" />`;
+	text += `<ul class="figure-list"><li>Horários: </li>`;
+	hs.map((item, index) => {
+		const imageSrc = urlBase.replace("[model]", model).replace("[variable]", variable).replace("[hs]", item);
+		const active = index == 0 ? " active" : "";
+		text += `<li><button class="figure-btn btn-open-image ${active}" id="figure-btn-${item}" onclick="openImage('${imageSrc}', 'figure-btn-${item}')">${item}</button></li>`;
+	});
+	text += `</ul>`;
+	content.innerHTML = text;
+
+	dialog.show();
+}
+
+function closeDialog() {
+	dialog.close();
 }
